722b6d609bffc03d5e39df068c78ef9d66b283ba
chris.godwin.ala <chris.godwin.ala@gmail.com>
2014-07-08 16:22:56 +1000

    Extracting a plugin from fieldcapture. Updated grails version to 2.3.11

---
+++ fieldcapture-hub/grails-app/conf/UrlMappings.groovy
@@ -0,0 +1,26 @@
+class UrlMappings {
+
+	static mappings = {
+		"/$controller/$action?/$id?"{
+			constraints {
+				// apply constraints here
+			}
+		}
+
+    "/$controller/$id?"(parseRequest:true) {
+        action = [GET: "get", POST: "upload", PUT: "upload", DELETE: "delete"]
+    }
+
+		"/"(controller: 'home', action: 'index')
+	//	"/about"(controller: 'home', action: 'about')
+    "/nocas/geoService"(controller: 'home', action: 'geoService')
+    "/myProfile"(controller: 'home', action: 'myProfile')
+    "/user/index"(controller: "user", action: "index")
+    "/user/checkEmailExists"(controller: "user", action: "checkEmailExists")
+    "/user/removeUserWithRole"(controller: "user", action: "removeUserWithRole")
+    "/user/addUserAsRoleToProject"(controller: "user", action: "addUserAsRoleToProject")
+    "/admin/user/${id}"(controller: "user", action: "show")
+		"500"(view:'/error')
+		"404"(view:'/error')
+	}
+}
