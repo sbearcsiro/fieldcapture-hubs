f63576a698f7995ce5cd46e47fcc52be4efe902d
chris.godwin.ala <chris.godwin.ala@gmail.com>
2014-07-22 10:49:48 +1000

    Added markers at the centroid of polygon sites based on an area threshold to make them more visible if they are small.

--- fieldcapture-hubs-plugin/web-app/js/mapWithFeatures.js
+++ fieldcapture-hubs-plugin/web-app/js/mapWithFeatures.js
@@ -487,16 +487,6 @@
 
             self.reset();
 
-        },
-        addLayer: function(layerName) {
-            var layerParams = [
-                "FORMAT=image/png8",
-                "LAYERS=ALA:" + layerName,
-                "STYLES=polygon"
-            ],
-            wms = new WMSTileLayer(layerName, map.wmsServer+'/gwc/service/wms?', layerParams, function(){}, 0.5);
-
-            map.map.overlayMapTypes.push(wms);
         }
     };
 
