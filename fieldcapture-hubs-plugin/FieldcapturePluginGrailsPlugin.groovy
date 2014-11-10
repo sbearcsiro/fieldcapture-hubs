cc432abffe7f3132f883b33e0c5537f67a190a97
chrisala <chris.godwin.ala@gmail.com>
2014-11-10 17:40:55 +1100

    Changed plugin grails version.  Changed fieldcapture version to SNAPSHOT.

--- fieldcapture-hubs-plugin/FieldcapturePluginGrailsPlugin.groovy
+++ fieldcapture-hubs-plugin/FieldcapturePluginGrailsPlugin.groovy
@@ -4,7 +4,7 @@ class FieldcapturePluginGrailsPlugin {
     // the plugin version
     def version = "0.1"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.2 > *"
+    def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
