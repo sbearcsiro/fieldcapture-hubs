59678b8c2ae16e889338b423c8ff634913053179
chrisala <chris.godwin.ala@gmail.com>
2014-11-13 16:13:44 +1100

    Configuring a build for the fieldcapture-hubs-plugin.

--- fieldcapture-hubs-plugin/grails-app/conf/Config.groovy
+++ fieldcapture-hubs-plugin/grails-app/conf/Config.groovy
@@ -31,7 +31,6 @@ println "[${appName}] (*) grails.config.locations = ${grails.config.locations}"
  \******************************************************************************/
 reloadable.cfgs = ["file:/data/${appName}/config/${appName}-config.properties"]
 
-grails.project.groupId = "au.org.ala." + appName // change this to alter the default package name and Maven publishing destination
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
 grails.mime.use.accept.header = false
 grails.mime.types = [
@@ -210,7 +209,7 @@ environments {
         ecodata.baseUrl = 'http://ecodata-test.ala.org.au/ws/'
         upload.images.url = grails.serverURL+'/image/'
         emailFilter = /[A-Z0-9._%-]+@csiro\.au|chris\.godwin\.ala@gmail.com/
-        logging.dir=/data/${appName}/log
+        //logging.dir=/data/${appName}/log
     }
     production {
         grails.logging.jul.usebridge = false
