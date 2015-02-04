07673dd15ff735d103134800998613ea75c76781
jamirali <jamirali@hotmail.com>
2015-02-04 14:46:18 +1100

    use ala-auth

--- fieldcapture-hubs-plugin/grails-app/conf/BuildConfig.groovy
+++ fieldcapture-hubs-plugin/grails-app/conf/BuildConfig.groovy
@@ -83,7 +83,7 @@ grails.project.dependency.resolution = {
         runtime (":rest:0.8") {
             excludes "httpclient", "httpcore"
         }
-        compile ":ala-auth-plugin:1.0.0"
+        compile ":ala-auth:1.0.0"
         runtime ":csv:0.3.1"
         compile ":markdown:1.1.1"
         compile ":resources:1.2.8"
