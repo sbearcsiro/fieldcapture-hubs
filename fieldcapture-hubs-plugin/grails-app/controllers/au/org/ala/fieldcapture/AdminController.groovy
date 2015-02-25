0aa06bf01f4796f54861e9ed094c5d7c951a4567
chrisala <chris.godwin.ala@gmail.com>
2015-02-25 17:06:20 +1100

    Moved the bulkUploadSites to the merit admin controller.

--- fieldcapture-hubs-plugin/grails-app/controllers/au/org/ala/fieldcapture/AdminController.groovy
+++ fieldcapture-hubs-plugin/grails-app/controllers/au/org/ala/fieldcapture/AdminController.groovy
@@ -487,22 +487,4 @@ class AdminController {
 
     }
 
-    @PreAuthorise(accessLevel = 'alaAdmin')
-    def bulkUploadSites() {
-
-        if (request.respondsTo('getFile')) {
-            def f = request.getFile('shapefile')
-
-            def result =  importService.bulkImportSites(f)
-
-            flash.message = result.message
-            render view:'tools'
-
-
-        } else {
-            flash.message = 'No shapefile attached'
-            render view:'tools'
-        }
-    }
-
 }
