0a16658b59380990a2348e8f2de240942e3c9d76
chrisala <chris.godwin.ala@gmail.com>
2015-03-03 09:35:50 +1100

    Added failed save detection to the MERI plan page.

--- fieldcapture-hubs-plugin/web-app/js/knockout-dates.js
+++ fieldcapture-hubs-plugin/web-app/js/knockout-dates.js
@@ -963,3 +963,27 @@ function activityProgressClass(progress) {
     return ACTIVITY_PROGRESS_CLASSES[progress];
 }
 
+ko.bindingHandlers.numeric = {
+    init: function (element, valueAccessor) {
+        $(element).on("keydown", function (event) {
+            // Allow: backspace, delete, tab, escape, and enter
+            if (event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || event.keyCode == 13 ||
+                    // Allow: Ctrl+A
+                (event.keyCode == 65 && event.ctrlKey === true) ||
+                    // Allow: . ,
+                (event.keyCode == 190 || event.keyCode == 110) ||
+                    // Allow: home, end, left, right
+                (event.keyCode >= 35 && event.keyCode <= 39)) {
+                // let it happen, don't do anything
+                return;
+            }
+            else {
+                // Ensure that it is a number and stop the keypress
+                if (event.shiftKey || (event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105)) {
+                    event.preventDefault();
+                }
+            }
+        });
+    }
+};
+
