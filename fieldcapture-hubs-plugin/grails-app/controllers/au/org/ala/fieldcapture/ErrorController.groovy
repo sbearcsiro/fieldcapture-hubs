44ffc80869d83737e40d55bbe549fe382460880a
chrisala <chris.godwin.ala@gmail.com>
2015-04-10 11:39:50 +1000

    Added recent hub features to the error page.

--- fieldcapture-hubs-plugin/grails-app/controllers/au/org/ala/fieldcapture/ErrorController.groovy
+++ fieldcapture-hubs-plugin/grails-app/controllers/au/org/ala/fieldcapture/ErrorController.groovy
@@ -4,13 +4,21 @@ class ErrorController {
 
     def settingService, cookieService
     def response404() {
-
-        def hub = cookieService.getCookie(SettingService.LAST_ACCESSED_HUB)
-        settingService.loadHubConfig(hub)
+        loadRecentHub()
         render view:'/404'
     }
 
     def response500() {
+        loadRecentHub()
         render view:'/error'
+    }
+
+    /**
+     * Loads the most recently accessed hub configuration so the error pages have access to the skin. (In the
+     * case of a 404 error, the hub may not be available for the current request).
+     */
+    private void loadRecentHub() {
+        def hub = cookieService.getCookie(SettingService.LAST_ACCESSED_HUB)
+        settingService.loadHubConfig(hub)
     }
 }
