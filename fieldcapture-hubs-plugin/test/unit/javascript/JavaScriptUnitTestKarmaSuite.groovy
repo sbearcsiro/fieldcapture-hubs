f30a83734ba3bd16af4a48131a3c7768cedd12fe
chrisala <chris.godwin.ala@gmail.com>
2015-05-22 14:26:08 +1000

    Added path to karma.

--- fieldcapture-hubs-plugin/test/unit/javascript/JavaScriptUnitTestKarmaSuite.groovy
+++ fieldcapture-hubs-plugin/test/unit/javascript/JavaScriptUnitTestKarmaSuite.groovy
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith
  * Runs jasmine tests using karma (via the Karma Test Runner plugin).
  */
 @RunWith(KarmaTestSuiteRunner.class)
+@KarmaTestSuiteRunner.KarmaStartupScripts("node_modules/karma/bin/karma")
 @KarmaTestSuiteRunner.KarmaConfigPath("test/unit/javascript/karma.ci.conf.js")
 class JavaScriptUnitTestKarmaSuite {
 }
