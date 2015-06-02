3857e1b9115a97bf43ab65d03a50dd2a7035be94
chrisala <chris.godwin.ala@gmail.com>
2015-06-03 09:15:03 +1000

    Minor progress on the functional test.

--- fieldcapture-hub/test/functional/au/org/ala/fieldcapture/CreateProjectSpec.groovy
+++ fieldcapture-hub/test/functional/au/org/ala/fieldcapture/CreateProjectSpec.groovy
@@ -19,15 +19,53 @@ class CreateProjectSpec extends FieldcaptureFunctionalTest {
         at EntryPage
     }
 
+    def "If the user is not a member of an organisation then they must select one via a search"() {
+        logout(browser)
+        login(browser, "fc-te@outlook.com", "testing!")
+
+        given: "navigate to the create project page"
+        to ProjectDetails, citizenScience:true
+
+        expect:
+        at ProjectDetails
+        organisation.organisationName == ''
+
+        when: "search for an organisation"
+        organisation.organisationName = "Test"
+
+        then: "The available organisations have been narrowed"
+
+
+        when: "an organisation is selected"
+        organisation.selectOrganisation("Test 1")
+
+        then: "The search field is disabled and updated to match the organisation name"
+        organisationName == "Test 1"
+        organisationName.is(":disabled")
+
+    }
+
     def "The user can create a citizen science project"() {
 
         logout(browser)
         login(browser, "fc-te@outlook.com", "testing!")
 
-        when: "navigate to the create project page"
-        to ProjectDetails
+        given: "navigate to the create project page"
+        to ProjectDetails, citizenScience:true
 
-        then: "blank"
+        expect:
         at ProjectDetails
+
+        when: "enter project details"
+        //projectType = 'Citizen Science Project' // The field should be pre-selected and disabled
+        recordUsingALA = 'Yes'
+        organisationName = 'Type Something here'
+
+        submit()
+
+        then: "at the newly created project page"
+        waitFor { at ProjectIndex }
+
+
     }
 }
--- fieldcapture-hub/test/functional/pages/ProjectDetails.groovy
+++ fieldcapture-hub/test/functional/pages/ProjectDetails.groovy
@@ -2,6 +2,8 @@ package pages
 
 import geb.Module
 import geb.Page
+import geb.navigator.Navigator
+import org.openqa.selenium.Keys
 
 class ProjectDetails extends Page {
 
@@ -10,33 +12,51 @@ class ProjectDetails extends Page {
     static at = { title == "Create | Project | Field Capture"}
 
     static content = {
-        projectType { module ProjectType }
+        projectType { $('[data-bind~=value:projectType]') }
+        recordUsingALA { $('[data-bind~=booleanValue:isExternal]') }
         organisation { module OrganisationSearch }
+        name { $('[data-bind~=value:name]') }
+        description { $('[data-bind~=value:name]') }
+        contactEmailAddress { $('') }
+        plannedStartDate { $('#plannedStartDate') }
+        plannedEndDate { $('#plannedEndDate') }
+        getInvolved { $('[data-bind~=value:getInvolved]') }
+        scienceType { $('[data-bind~=value:scienceType]') }
+
+        saveButton { $('#save') }
+        cancelButton { $('#cancel') }
+
     }
 
+    def setDate(Navigator dateField, String date) {
+        dateField.value(date)
+        dateField << Keys.chord(Keys.ENTER) // Dismisses the popup calendar
+    }
     // Pressing submit actually does an ajax call then changes the page using JavaScript.
     def save() {
         saveButton.click()
     }
     def cancel() {
-
+        cancelButton.click()
     }
 }
 
-class ProjectType extends Module {
-
-    static content = {
-        type { $('[data-bind~=value:projectType]') }
-        recordUsingALA { $('[data-bind~=value:isExternal]') }
-    }
-}
 
 class OrganisationSearch extends Module {
     static content = {
-        organisationName { $('[]')}
+        organisationName { $('#organisationName')}
         results {}
-        notOnList {}
-        registerButton {}
+        notOnList { $('[data-bind~=checked:organisationNotPresent') }
+        clearButton {$('[data-bind~=click:clearSelection')}
+        registerButton {$('#registerOrganisation')}
     }
+    def clearSelection() {
+        clearButton.click()
+    }
+
+    def selectOrganisation(name) {
+        $('a [data-bind~=text:'+name+']').click()
+    }
+
 }
 
